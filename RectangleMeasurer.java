e0a655997b36637844eae506c42635022d1053db
Kyle McDowell <kymcd21@icstudents.org>
2021-02-07 01:55:52 +0000

    Initial commit

---
+++ RectangleMeasurer.java
@@ -0,0 +1,15 @@
+import java.awt.Rectangle;
+
+/**
+   Objects of this class measure rectangles by area.
+*/
+public class RectangleMeasurer implements Measurer
+{
+   public double measure(Object anObject)
+   {
+      Rectangle aRectangle = (Rectangle) anObject;
+      double area = aRectangle.getWidth() * aRectangle.getHeight();
+      System.out.println(area);
+      return area;
+   }
+}
